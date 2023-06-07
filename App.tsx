f80254bb88bb47093289fdc0caf0bd388ebc97ae
bilmoleque2 <gabriel.bil8@gmail.com>
2023-06-06 23:28:44 -0300

    Locale do moment definido

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import { useEffect } from 'react';
 import { NativeRouter } from 'react-router-native';
 import { AuthProvider } from './src/context/Auth';
 import { ThemeProvider } from './src/context/Theme';
 import { AllRoutes } from './src/routes/AllRoutes';
 
 export default function App() {
+	useEffect(() => {
+		moment.locale('pt-br');
+	}, []);
+
 	return (
 		<NativeRouter>
 			<AuthProvider>
